497c60c1443ae9d08cd8cd09835a0af87afb145d
CoraxTR <obsistway@gmail.com>
2025-05-08 21:56:10 +0300

    Added basic orderfetching

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"mstorefgo/internal/config"
+	moyskladapi "mstorefgo/internal/moyskladapi"
+)
+
 func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		panic(".env not ready")
+	}
+	MSRateLimiter := moyskladapi.NewRatelimiter(cfg.RequestCap, cfg.TimeSpan)
+	msprocessor := moyskladapi.NewMoySkladProcessor(MSRateLimiter, &cfg.Moyskladapiconfig)
+
+	msprocessor.GetDeliverableOrders()
 
 }
